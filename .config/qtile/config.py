fa88a123cc89f639d044da00c50b28158d9195b1
Dustin Pilgrim <dustin.pilgrim1997@gmail.com>
2023-11-19 21:26:19 -0400

    Update qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -297,10 +297,6 @@ screens = [
             # border_color="#1e222a",
             margin=10,
         ),
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
     ),
 
     #
